8035b6a32c37d736490745af7f4faef48e8dd14a
liverbool <nukboon@gmail.com>
2017-05-04 03:55:31 +0700

    remove logout routing

--- bootstrap.php
+++ bootstrap.php
@@ -9,6 +9,5 @@ use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
     $events->subscribe(AddApiDataListener::class);
     $events->subscribe(AddAssetListener::class);
-    $events->subscribe(AuthenRoutingListener::class);
     $events->subscribe(DiscussionThaiSlugListener::class);
 };

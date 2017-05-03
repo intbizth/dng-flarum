ecd86392125236ba674da111e099ad9368710752
liverbool <nukboon@gmail.com>
2017-05-04 02:25:33 +0700

    update

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -2,7 +2,6 @@ import {extend} from 'flarum/extend';
 import HeaderPrimary from 'flarum/components/HeaderPrimary';
 import HeaderSecondary from 'flarum/components/HeaderSecondary';
 import SettingsPage from 'flarum/components/SettingsPage';
-import SessionDropdown from 'flarum/components/SessionDropdown';
 import LogInModal from 'flarum/components/LogInModal';
 import DiscussionHero from 'flarum/components/DiscussionHero';
 import CommentPost from 'flarum/components/CommentPost';
@@ -33,14 +32,6 @@ app.initializers.add('toro-dng', function () {
     });
 
     extend(HeaderSecondary.prototype, 'items', function (items) {
-        if (items.has('signUp')) {
-            items.remove('signUp');
-        }
-
-        if (items.has('logIn')) {
-            items.remove('logIn');
-        }
-
         addLinks(items, 'secondary');
     });
 
@@ -50,17 +41,11 @@ app.initializers.add('toro-dng', function () {
         }
     });
 
-    extend(SessionDropdown.prototype, 'items', function (items) {
-        if (items.has('logOut')) {
-            items.remove('logOut');
-        }
-    });
-
-    LogInModal.prototype.content = function() {
+    /*LogInModal.prototype.content = function() {
         window.location.href = app.forum.attribute('dng.login');
         // TODO: custom dng login form
         return [];
-    }
+    }*/
 
     DiscussionHero.prototype.view = function() {
         const discussion = this.props.discussion;
--- src/AuthenRoutingListener.php
+++ src/AuthenRoutingListener.php
@@ -5,7 +5,6 @@ namespace Dng\Flarum;
 use Dng\Flarum\LoginController;
 use Dng\Flarum\LogoutController;
 use Flarum\Event\ConfigureForumRoutes;
-use Flarum\Extension\ExtensionManager;
 use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Contracts\Events\Dispatcher;
 use Symfony\Component\Yaml\Yaml;
@@ -17,14 +16,8 @@ class AuthenRoutingListener
      */
     private $settings;
 
-    /**
-     * @var ExtensionManager
-     */
-    private $extensionManager;
-
-    public function __construct(ExtensionManager $extensionManager, SettingsRepositoryInterface $settings)
+    public function __construct(SettingsRepositoryInterface $settings)
     {
-        $this->extensionManager = $extensionManager;
         $this->settings = $settings;
     }
 
@@ -41,27 +34,15 @@ class AuthenRoutingListener
      */
     public function onConfigureForumRoutes(ConfigureForumRoutes $event)
     {
-        $ext = $this->extensionManager->getExtension(DngForum::NAME);
-        $settings = (array) $ext->composerJsonAttribute('extra.flarum-extension.settings');
-        $loginUrl = $settings['login_url'];
-        $logoutUrl = $settings['logout_url'];
-
         // todo: setting provider or db setttings
         if ($file = $this->settings->get(DngForum::SETTING_FILE_KEY, __DIR__ . '../../../../dng.settings.yml')) {
             if (file_exists($file)) {
                 $settings = Yaml::parse(file_get_contents($file));
 
-                if (!empty($settings['login_url'])) {
-                    $loginUrl = $settings['login_url'];
-                }
-
                 if (!empty($settings['logout_url'])) {
-                    $logoutUrl = $settings['logout_url'];
+                    $event->get($settings['logout_url'], 'dngLogout', LogoutController::class);
                 }
             }
         }
-
-        $event->post($loginUrl, 'dngLogin', LoginController::class);
-        $event->get($logoutUrl, 'dngLogout', LogoutController::class);
     }
 }

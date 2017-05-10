653cefd1d1a58b906b350f172d56d9ade50a66c5
liverbool <nukboon@gmail.com>
2017-05-10 23:23:20 +0700

    fix slug

--- src/DiscussionThaiSlugListener.php
+++ src/DiscussionThaiSlugListener.php
@@ -31,25 +31,6 @@ class DiscussionThaiSlugListener
      */
     public static function slug($text, $len = 60)
     {
-        /*$string = preg_replace('/[^a-z0-9ก-ฮแเใไโฯๆะาอิอีอึอือุอูอ์อ่อ้อ๊อ๋อัอฺอำอํ]/i', '-', $text);
-        $string = preg_replace('/-+/', '-', $string);
-        $string = preg_replace('/-$|^-/', '', $string);
-        $string = strtolower($string);
-
-        return $string ?: $text;*/
-
-        // https://github.com/bryanbraun/anchorjs/blob/master/anchor.js#L221
-        // Regex for finding the nonsafe URL characters (many need escaping): & +$,:;=?@"#{}|^~[`%!']./()*\
-        $nonsafeChars = preg_quote("&+$,:;=?@\"#{}|^~[`%!'].()*\\");
-
-        $string = trim($text);
-        $string = preg_replace("/$nonsafeChars\/\s+/", '-', $string);
-        $string = preg_replace('/-{2,}/', '-', $string);
-        $string = substr($string, 0, $len);
-        $string = preg_replace('/^-+|-+$/', '', $string);
-        $string = strtolower($string);
-        $string = trim($string);
-
-        return $string ?: $text;
+        return URLify::slug($text, $len);
     }
 }
